c0e7f02e610892063399dcaac4d394739bf9f1cd
ferror <zmalcherczyk@gmail.com>
2021-03-02 18:07:19 +0100

    documents

--- src/Search/DocumentField.php
+++ src/Search/DocumentField.php
@@ -5,5 +5,27 @@ namespace Landingi\AwsBundle\Search;
 
 final class DocumentField
 {
+    private string $key;
+    private string $value;
 
+    public function __construct(string $key, string $value)
+    {
+        $this->key = $key;
+        $this->value = $value;
+    }
+
+    public function toArray(): array
+    {
+        return [$this->key => $this->value];
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
 }
--- src/Search/DocumentIdentifier.php
+++ src/Search/DocumentIdentifier.php
@@ -5,4 +5,15 @@ namespace Landingi\AwsBundle\Search;
 
 final class DocumentIdentifier
 {
+    private string $identifier;
+
+    public function __construct(string $identifier)
+    {
+        $this->identifier = $identifier;
+    }
+
+    public function getString(): string
+    {
+        return $this->identifier;
+    }
 }
