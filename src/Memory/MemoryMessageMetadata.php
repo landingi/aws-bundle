8dc55dbc1e32166e0bf459338b2ef17badafd5d2
ferror <zmalcherczyk@gmail.com>
2021-03-16 16:14:34 +0100

    More memory tests

--- src/Memory/MemoryMessageMetadata.php
+++ src/Memory/MemoryMessageMetadata.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 namespace Landingi\AwsBundle\Memory;
 
 use Landingi\AwsBundle\Queue\MessageMetadata;
+use Landingi\AwsBundle\Queue\QueueException;
 
 final class MemoryMessageMetadata implements MessageMetadata
 {
     private int $delay;
 
+    /**
+     * @throws QueueException
+     */
     public function __construct(int $delay)
     {
+        if ($delay < 0) {
+            throw new QueueException('Invalid Metadata value');
+        }
+
         $this->delay = $delay;
     }
 
---
+++ tests/Memory/MemoryMessageMetadataTest.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\AwsBundle\Memory;
+
+use Landingi\AwsBundle\Queue\QueueException;
+use PHPUnit\Framework\TestCase;
+
+final class MemoryMessageMetadataTest extends TestCase
+{
+    public function testItSerializes(): void
+    {
+        self::assertEquals(0, (new MemoryMessageMetadata(0))->getDelay());
+        self::assertEquals(1, (new MemoryMessageMetadata(1))->getDelay());
+        self::assertEquals(200, (new MemoryMessageMetadata(200))->getDelay());
+    }
+
+    public function testItThrowsExceptionForInvalidMetadata(): void
+    {
+        $this->expectException(QueueException::class);
+        new MemoryMessageMetadata(0);
+    }
+}
