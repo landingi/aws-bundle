5f0701634fffc19521efcfbbdfa99780237fbf63
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2021-04-26 15:08:23 +0200

    Add required $endpoint parameter to dynamodb (#21)
    Required $endpoint parameter to use the local environment like http://dynamodb:8000 it is already in docs.

--- src/Aws/DynamoDb/ClientFactory.php
+++ src/Aws/DynamoDb/ClientFactory.php
@@ -8,12 +8,18 @@ use Aws\DynamoDb\DynamoDbClient;
 
 final class ClientFactory
 {
-    public function build(Credentials $credentials, string $region): DynamoDbClient
+    public function build(Credentials $credentials, string $region, string $endpoint): DynamoDbClient
     {
-        return new DynamoDbClient([
+        $config = [
             'credentials' => $credentials,
             'region' => $region,
             'version' => 'latest',
-        ]);
+        ];
+
+        if ($endpoint) {
+            $config['endpoint'] = $endpoint;
+        }
+
+        return new DynamoDbClient($config);
     }
 }
