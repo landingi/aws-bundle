f70322068a448ae61eb36b27ba8f536865b9994f
rafal-strzelbicki <rafal.strzelbicki@gmail.com>
2021-05-11 10:05:10 +0200

    test me baby one more time

---
+++ tests/Aws/TimeStream/ClientFactoryTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\AwsBundle\Aws\TimeStream;
+
+use Aws\Credentials\Credentials;
+use Aws\TimestreamWrite\TimestreamWriteClient;
+use PHPUnit\Framework\TestCase;
+
+class ClientFactoryTest extends TestCase
+{
+    public function testBuild(): void
+    {
+        // arrange
+        $factory = new ClientFactory();
+        $credentials = new Credentials('key', 'secret');
+        $region = 'region';
+
+        // act & assert
+        self::assertEquals(
+            new TimeStreamClient(
+                new TimestreamWriteClient([
+                    'credentials' => $credentials,
+                    'region' => $region,
+                    'version' => 'latest',
+                ])
+            ),
+            $factory->build(
+                $credentials,
+                $region
+            )
+        );
+    }
+}
